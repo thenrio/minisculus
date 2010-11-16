84200c26fc6f4de922937ca7e68be119c9d7037b
thierry <thierry.henrio@gmail.com>
2010-11-16 22:43:23 +0100

    method name are challenging

--- spec/minisculus/challenge_spec.rb
+++ spec/minisculus/challenge_spec.rb
@@ -1,32 +1,37 @@
 require 'spec_helper'
 require 'restclient'
-require 'ruby-debug'
 require 'yajl'
+require 'logger'
 
 module Challenge
+  RestClient.log = Logger.new(STDOUT)
   def self.base
     'http://minisculus.edendevelopment.co.uk'
   end
 
-  def get_json(page)
-    response = RestClient.get("#{base}/#{page}", {:accept => :json})
-    Yajl::Parser.new.parse(response)
+  def show_question_under_jsoned_secret(page)
+    RestClient.get("#{base}#{reveal_jsoned_secret(page)}")
   end
-  module_function :get_json
+  module_function :show_question_under_jsoned_secret
+
+  def reveal_jsoned_secret(page)
+    response = RestClient.get("#{base}#{page}", {:accept => :json})
+    Yajl::Parser.new.parse(response)['reference-url']
+  end
+  module_function :reveal_jsoned_secret
 end
 
 describe Challenge do
-  describe "#get_json" do
-    it 'should get a json hash' do
-       response = Challenge.get_json('start')
-       assert {response['reference-url'] == '/questions/14f7ca5f6ff1a5afb9032aa5e533ad95.html'}
+  describe "#reveal_jsoned_secret" do
+    it 'should get a json hash and return secret key' do
+       assert {Challenge.reveal_jsoned_secret('/start') == '/questions/14f7ca5f6ff1a5afb9032aa5e533ad95.html'}
     end
   end
   
-  describe '#show' do
+  describe '#show_question_under_jsoned_secret' do
     it 'should show the page under reference-url token' do
-      
+      page = Challenge.show_question_under_jsoned_secret('/start')
+      assert {page =~ /FIRST CHALLENGE/}
     end
   end
-  
 end
