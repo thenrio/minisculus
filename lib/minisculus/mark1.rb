1dc4bcf49b36118d6305637ec49e208136145d60
thierry <thierry.henrio@gmail.com>
2010-11-17 00:27:06 +0100

    extract mark1

---
+++ lib/minisculus/mark1.rb
@@ -0,0 +1,23 @@
+module Engines
+  class Mark_I
+    attr_accessor :position
+    def initialize(position=5)
+      self.position = position
+    end
+    def code(secret)
+      secret.chars.map {|c| letters[(letters.index(c) + position)%letters.length]}.join
+    end
+    
+    private
+    def letters
+      @@letters ||= [
+        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+        "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+        "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+        ".", ",", "?", "!", "'", "\"", " "
+      ]
+    end
+  end
+end
