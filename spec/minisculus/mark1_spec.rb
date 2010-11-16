3e3c4ccb82f6e1ddeff9738f9f57e983e7df25cc
thierry <thierry.henrio@gmail.com>
2010-11-17 00:15:59 +0100

    spec a mark I engine

--- spec/minisculus/mark1_spec.rb
+++ spec/minisculus/mark1_spec.rb
@@ -1,8 +1,38 @@
 require 'spec_helper'
 
-module Mark_I
+module Engines
+  class Mark_I
+    attr_accessor :position
+    def initialize(position=5)
+      self.position = position
+    end
+    def code(secret)
+      secret.chars.map {|c| letters[(letters.index(c) + position)%letters.length]}.join
+    end
+    
+    private
+    def letters
+      @@letters ||= [
+        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+        "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+        "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+        ".", ",", "?", "!", "'", "\"", " "
+      ]
+    end
+  end
 end
 
-describe Mark_I do
-  
+describe Engines::Mark_I do
+  describe '#code' do
+    let(:engine) {Engines::Mark_I.new}
+    
+    it {
+      assert {engine.code('ac') == 'fh'}
+    }
+    it {
+      assert {engine.code(' ') == '4'}
+    }
+  end
 end
