e907c7eb1f54dfb30add44c17c8af4e878c4819c
thierry <thierry.henrio@gmail.com>
2010-11-17 21:36:26 +0100

    spec answer to a question is a next question when redirected

--- spec/minisculus/question_spec.rb
+++ spec/minisculus/question_spec.rb
@@ -2,6 +2,13 @@ require 'spec_helper'
 require 'minisculus/question'
 
 describe Minisculus::Question do
+  describe '.headers' do
+    it 'should set accept and content type as json' do
+      assert {Minisculus::Question.headers['Accept'] == 'application/json'}
+      assert {Minisculus::Question.headers['Content-Type'] == 'application/json'}
+    end
+  end
+  
   describe '#read' do
     let(:question) {Minisculus::Question.new('/1')}
     
@@ -25,13 +32,23 @@ describe Minisculus::Question do
       q
     }
     
-    it 'put to minisculus site, as json, transformed message using given block' do
+    it 'puts to minisculus site, as json, transformed message using given block' do
       content = '{"answer":"ha"}'
       mock(Typhoeus::Request).put(question.uri, :body => content, :headers => Minisculus::Question.headers) {
         Typhoeus::Response.new(:code => 303)
       }
-      response = question.answer {message}
-      assert {response.code == 303}
+      question.answer {message}
+    end
+    
+    describe 'when response is redirect' do
+      it 'should return a new question' do
+        mock(Typhoeus::Request).put(anything, anything) {
+          Typhoeus::Response.new(:code => 303, :headers => {'Location' => '/next-question'})
+        }
+        next_question = question.answer {}
+        assert {next_question.class == Minisculus::Question}
+        assert {next_question.uri == '/next-question'}
+      end
     end
   end
 end
