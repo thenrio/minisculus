600fa761fff22c5191b79dd69c8b5a782985976c
thierry <thierry.henrio@gmail.com>
2010-11-17 21:00:01 +0100

    sort, shine

--- spec/minisculus/question_spec.rb
+++ spec/minisculus/question_spec.rb
@@ -4,31 +4,35 @@ require 'yajl'
 
 module Minisculus  
   class Question
-    attr_accessor :title, :content_url, :message
-    def initialize(title)
-      self.title = title
+    attr_accessor :uri, :content_url, :message
+    def initialize(uri)
+      self.uri = uri
     end
     
     def read
-      s = Typhoeus::Request.get(question_url, :headers => headers)
+      s = Typhoeus::Request.get(uri, :headers => headers)
       hash = Yajl::Parser.new.parse(s)
       self.content_url = hash['reference-url']
       self.message = hash['question']
+      self
     end
     
     def answer(&block)
       answer = yield message
       content = Yajl::Encoder.encode({'answer' => answer})
-      Typhoeus::Request.put(question_url, :body => content, :headers => headers)
+      response = Typhoeus::Request.put(uri, :body => content, :headers => headers)
     end
 
     def self.headers
       {'Accept' => 'application/json', 'Content-Type' => 'application/json'}
     end
 
-    private
-    def question_url
-      "http://minisculus.edendevelopment.co.uk#{title}"
+    def uri
+      if @uri =~ /^http:/
+        @uri
+      else
+        "http://minisculus.edendevelopment.co.uk#{@uri}"
+      end 
     end
     
     def headers
@@ -56,19 +60,19 @@ describe Minisculus::Question do
   
   describe '#answer' do
     let(:question) {
-      q = Minisculus::Question.new('/start')
+      q = Minisculus::Question.new('/234')
       q.content_url = 'oh'; q.message = 'ha'
       q
     }
     
     it 'put to minisculus site, as json, transformed message using given block' do
-      url = 'http://minisculus.edendevelopment.co.uk/start'
+      url = 'http://minisculus.edendevelopment.co.uk/234'
       content = '{"answer":"code"}'
       mock(Typhoeus::Request).put(url, :body => content, :headers => Minisculus::Question.headers) {
-        '???'
+        Typhoeus::Response.new(:code => 303)
       }
       response = question.answer {|message| 'code'}
-      assert {response == '???'}
+      assert {response.code == 303}
     end
   end
 end
