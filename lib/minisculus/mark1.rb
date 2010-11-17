ec0b5aeed400b939f8f1b807a77e7368d837b356
thierry <thierry.henrio@gmail.com>
2010-11-17 11:17:27 +0100

    decode(encode(thing)) == thing use map|reduce style with inject (no list comprehension as erlang)

--- lib/minisculus/mark1.rb
+++ lib/minisculus/mark1.rb
@@ -35,8 +35,11 @@ module Engines
       self.position = position
       self.wheel = Wheel.new
     end
-    def encode(secret)
-      secret.chars.map {|c| wheel.move(c).shift(position).read}.join
+    def encode(secret, position = self.position)
+      secret.chars.inject('') {|acc,c| acc << wheel.move(c).shift(position).read}
+    end
+    def decode(secret)
+      encode(secret, -self.position)
     end
   end
 end
--- spec/minisculus/mark1_spec.rb
+++ spec/minisculus/mark1_spec.rb
@@ -13,4 +13,13 @@ describe Engines::Mark_I do
       assert {engine.encode(' ') == '4'}
     }
   end
+  
+  describe '#decode' do
+    let(:engine) {Engines::Mark_I.new}
+    
+    it 'read an encoded string' do
+      secret = 'blue'
+      assert {engine.decode(engine.encode(secret)) == secret}
+    end
+  end
 end
