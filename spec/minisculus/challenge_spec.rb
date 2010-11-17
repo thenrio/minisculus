0dbac58232f5fd414e825992ed8137496fe37b89
thierry <thierry.henrio@gmail.com>
2010-11-17 10:47:22 +0100

    better response at this time for an answer is RestClient.put('http://minisculus.edendevelopment.co.uk/start', ans, {:content_type => :json, :accept => :json}) RestClient::NotAcceptable: 406 Not Acceptable

--- spec/minisculus/challenge_spec.rb
+++ spec/minisculus/challenge_spec.rb
@@ -14,4 +14,16 @@ describe Challenge do
       assert {page =~ /FIRST CHALLENGE/}
     end
   end
+  
+  def answer_to_jsoned_secret(question, answer)
+    answer = Yajl::Encoder.encode({'answer' => answer})
+    RestClient.put("#{Challenge.base}#{question}", answer, :content_type => :json, :accept => :json)
+  end
+  
+  describe '#answer_to_jsoned_secret' do
+    it 'receive a redirect code for a correct answer' do
+      response = answer_to_jsoned_secret('/questions/14f7ca5f6ff1a5afb9032aa5e533ad95', 'Yzxutm5TK5cotjy2')
+      pp response
+    end
+  end
 end
