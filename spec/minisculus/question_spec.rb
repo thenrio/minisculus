68160a5898c2fb9ba212d8c97ccf9909b3a51ef4
thierry <thierry.henrio@gmail.com>
2010-11-17 13:18:52 +0100

    remove fake implementation make shorter test strings

--- spec/minisculus/question_spec.rb
+++ spec/minisculus/question_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require 'restclient'
+require 'yajl'
 
 module Minisculus
   class Question
@@ -8,8 +9,10 @@ module Minisculus
       self.title = title
     end
     def read
-      self.content_url = 'http://minisculus.edendevelopment.co.uk/questions/14f7ca5f6ff1a5afb9032aa5e533ad95.html'
-      self.message = 'Strong NE Winds!'
+      s = RestClient.get("http://minisculus.edendevelopment.co.uk#{title}", :accept => :json)
+      hash = Yajl::Parser.new.parse(s)
+      self.content_url = hash['reference-url']
+      self.message = hash['question']
     end
   end
 end
@@ -19,14 +22,15 @@ describe Minisculus::Question do
     let(:question) {Minisculus::Question.new('/start')}
     
     it 'get page from minisculus site' do
+      url, message = "oh", "ha"
       stub(RestClient).get('http://minisculus.edendevelopment.co.uk/start', :accept => :json) {
-        "{\"reference-url\":\"/questions/14f7ca5f6ff1a5afb9032aa5e533ad95.html\",\"question\":\"Strong NE Winds!\"}"
+        "{\"reference-url\":\"#{url}\",\"question\":\"#{message}\"}"
       }
 
       question.read()
 
-      assert {question.content_url == 'http://minisculus.edendevelopment.co.uk/questions/14f7ca5f6ff1a5afb9032aa5e533ad95.html'}
-      assert {question.message == 'Strong NE Winds!'}
+      assert {question.content_url == 'oh'}
+      assert {question.message == 'ha'}
     end
   end
 end
