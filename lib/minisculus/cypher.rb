bb42b3729937b02e6c9935530bc76d68ea962560
thierry <thierry.henrio@gmail.com>
2010-11-18 18:36:25 +0100

    hum that does not suffice to get green ...

--- lib/minisculus/cypher.rb
+++ lib/minisculus/cypher.rb
@@ -3,35 +3,42 @@ require 'minisculus/wheel'
 module Cypher
   module Device
     class ShiftingWheel
-      attr_accessor :offset, :wheel
-      def initialize(offset=5)
+      attr_accessor :offset
+      def initialize(offset=5, charset=Engines::Wheel.letters)
         self.offset = offset
-        self.wheel = Engines::Wheel.new
-      end
-      
-      def encode(secret, offset=self.offset)
-        secret.chars.inject('') {|acc,c| acc << wheel.move(c).shift(offset).read}
-      end
-      def decode(secret)
-        encode(secret, -offset)
-      end      
-    end
-    
-    class SelfTurningWheel
-      def initialize(charset=Engines::Wheel.letters)
         @wheel = Engines::Wheel.new(charset)
       end
       
+      def encode(secret, offset=self.offset)
+        secret.chars.inject('') {|acc,c| acc << crypt(c, offset)}
+      end
+      def decode(secret)
+        encode(secret, -offset)
+      end
+      
+      protected
+      def crypt(c, offset)
+        @wheel.move(c).shift(offset).read
+      end   
+    end
+    
+    class SelfTurningWheel < ShiftingWheel
+      def initialize(charset=Engines::Wheel.letters)
+        super(0, charset)
+      end
+      
       def encode(secret)
+        self.offset = 0
         secret.chars.inject('') {|acc, c|
-          c = @wheel.move(c).shift(offset(acc[-1])).read unless acc.empty?
-          acc << c
+          acc << crypt(c, offset)
+          turn(acc[-1])
+          acc
         }
       end
       
       private
-      def offset(char)
-        @wheel.letters.index(char) * 2
+      def turn(char)
+        self.offset += @wheel.letters.index(char) * 2
       end
     end
     
--- spec/minisculus/cypher_spec.rb
+++ spec/minisculus/cypher_spec.rb
@@ -5,16 +5,14 @@ describe Cypher::Device::SelfTurningWheel do
   CHARSET = %w(0 1 2 3)
   let(:will) {Cypher::Device::SelfTurningWheel.new(CHARSET)}
   it 'has initial position 0' do
-    assert {will.position == 0}
+    assert {will.offset == 0}
   end
   
   describe "#encode" do
     context 'one char' do
-      it 'first character is the same' do
+      it 'first character is the same, and offset has moved of 2' do
         assert {will.encode('1') == '1'}
-      end
-      it 'has a new position' do
-        assert {will.position == 2}
+        assert {will.offset == 2}
       end
     end
     
@@ -22,6 +20,10 @@ describe Cypher::Device::SelfTurningWheel do
       it 'second is shift of 2 * "index of previous in charset"' do
         assert {will.encode('11') == '13'}
       end      
+    end
+    
+    it 'encoding twice same string produce same result' do
+      assert {will.encode('1') == will.encode('1')}
     end
   end
 end
