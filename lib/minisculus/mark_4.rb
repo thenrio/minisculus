46c6b2011fbf13721575eb007a1304604098137d
thierry <thierry.henrio@gmail.com>
2010-11-18 01:50:13 +0100

    implement it ? more at ease with array than string, hence a join operation ...

--- lib/minisculus/mark_4.rb
+++ lib/minisculus/mark_4.rb
@@ -3,10 +3,16 @@ module Engines
   class Mark_IV
     def initialize(offsets=[4,7])
       @mark_II = Engines::Mark_II.new(offsets)
+      @wheel = Engines::Wheel.new
     end
     
     def encode(secret)
       secret = @mark_II.encode(secret)
+      secret.chars.inject([]) {|acc, c|
+        c = @wheel.move(c).shift(@wheel.letters.index(acc.last) * 2).read if acc.last
+        acc << c
+        acc
+      }.join('')
     end
   end
 end
