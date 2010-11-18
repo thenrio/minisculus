ee19c024b2c99889fa84e5ce82f7127c0ee9b4ca
thierry <thierry.henrio@gmail.com>
2010-11-18 01:00:55 +0100

    tail end recursion is a pattern just use it right!

--- lib/minisculus/mark_2.rb
+++ lib/minisculus/mark_2.rb
@@ -1,22 +1,23 @@
-require 'minisculus/wheel'
+require 'minisculus/mark_1'
 
 module Engines
   class Mark_II
     def initialize(offsets = [2, 5])
-      @offsets = offsets
-      @wheel = Engines::Wheel.new
+      @marks = offsets.inject([]) {|acc, o| 
+        o = (acc.length+1)*o*(acc.length.even? ? 1 : -1)
+        acc << Engines::Mark_I.new(o)
+        acc
+      }
     end
     
     def encode(secret)
-      _encode(secret, @offsets)
+      _encode(secret, @marks)
     end
     
-    def _encode(secret, offsets)
-      debugger
-      offset = offsets.shift
-      if offset
-        s = secret.chars.inject('') {|acc, c| acc << @wheel.move(c).shift(offset).read}
-        _encode(s, offsets)
+    def _encode(secret, marks)
+      mark = marks.shift
+      if mark
+        _encode(mark.encode(secret), marks)
       else
         secret
       end
