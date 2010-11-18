c4961f285dbc8b47a13a1a2c828750cacec734b0
thierry <thierry.henrio@gmail.com>
2010-11-18 11:59:27 +0100

    tell

--- spec/minisculus/cypher_spec.rb
+++ spec/minisculus/cypher_spec.rb
@@ -5,8 +5,11 @@ describe Cypher::Device::Will_III do
   let(:will) {Cypher::Device::Will_III.new(%w(0 1 2 3))}
   
   describe "#encode" do
-    it 'does not touch first character' do
-      assert {will.encode('2') == '2'}
+    it 'first character is the same' do
+      assert {will.encode('1') == '1'}
+    end
+    it 'second is shift of 2 * "index of previous in charset"' do
+      assert {will.encode('11') == '13'}
     end
   end
 end
