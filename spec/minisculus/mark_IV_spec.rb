b8b4b388d0740337d0793bbfb11748ea3942ded0
thierry <thierry.henrio@gmail.com>
2010-11-19 14:22:46 +0100

    spec this machine

---
+++ spec/minisculus/mark_IV_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+require 'minisculus/engines'
+
+describe "Mark_IV" do
+  let(:machine) {Engines.mark_IV}
+  it 'is reversible, as "decode(encode(secret)) == secret"' do
+    assert {machine.encode('The') == 'JMl'}
+    # assert {machine.decode('JMl') == 'The'}
+  end
+end
